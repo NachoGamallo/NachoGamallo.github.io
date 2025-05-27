9398a78a7d90e35aa1c85377a3e5b0aaac591887
NASHE\shote <nachogamallolafon123@gmail.com>
2025-05-27 07:28:11 +0200

    Last changes in the index, 3th with JS

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 setTimeout(() => {
-    alert("¡Hola! Bienvenido a la página del 3er Trimestre.");
+    alert("¡Hola! Bienvenido a la página del 3er Trimestre con JS implementado!!.");
 }, 2500); /* Popup despues de 2500 ms (2.5 segundos )*/
 
-/* Cuando hagamos click en una imagen, esta cambiará. */
+/* Cuando hagamos click en una imagen, esta cambiará. Guardamos la foto original en una variable, al pasar 5 segundos sin clicar vuelve a la original. (Aun se puede mejorar)*/
 document.addEventListener('DOMContentLoaded', function () {
+    
     const img = document.getElementById('imgJS');
 
+    const imgOriginal = img.src;
+
+    let timeoutId = null; //Guardamos temporizador.
+
+    const imgNueva = "https://i.pinimg.com/originals/87/8c/53/878c5376b10b2f842a06fae2d386c463.gif";
+
     img.addEventListener('click', () => {
-        img.src = "";
+        img.src = imgNueva;
+
+        if (timeoutId){
+            clearTimeout(timeoutId);
+        }//Si el usuario clica y ya habia un temporizador en marcha, se borra y empieza de 0.
+
+        setTimeout(() => {
+            img.src = imgOriginal;
+            timeoutId = null;
+        }, 5000)//Tras 5 segundos, cambia a la imagen inicial.
     });
 });
 
