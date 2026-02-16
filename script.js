da2d47b11053eecfa295dcb9033e8aa7228f35cb
Nacho Gamallo <nachogamallolafon123@gmail.com>
2026-02-16 20:51:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    if (slides.length > 0) {
+        setInterval(() => {
+            slides[currentSlide].classList.remove('active');
+            currentSlide = (currentSlide + 1) % slides.length;
+            slides[currentSlide].classList.add('active');
+        }, 3000); // Cambia cada 4 segundos
+    }
+});
